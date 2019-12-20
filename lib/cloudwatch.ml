62805b8c741a5cfa1de8ed42a2d5e34bae4e8457
Jan Rochel <jrochel@besport.com>
2019-12-20 11:09:43 +0100

    cloudwatch

---
+++ lib/cloudwatch.ml
@@ -0,0 +1,80 @@
+let endpoint region =
+  Printf.sprintf "monitoring.%s.amazonaws.com" (Common.Region.to_string region)
+
+let action act = [
+  "Version", "2010-08-01";
+  "Action", act
+]
+
+module Make (Conf : Service.CONF) = struct
+
+  module Service = Service.Make (Conf)
+    (struct let name = "monitoring" and host = endpoint Conf.region end)
+
+  type stat = SampleCount | Average | Sum | Minimum | Maximum
+
+  let show_stat = function
+    | SampleCount -> "SampleCount"
+    | Average -> "Average"
+    | Sum -> "Sum"
+    | Minimum -> "Minimum"
+    | Maximum -> "Maximum"
+
+  let get_metric_statistics ~namespace ~metric_name ?(dimensions = [])
+                            ~statistics ~start_time ~end_time ~period =
+
+    assert (List.mem period [1; 5; 10; 30] || period mod 60 = 0);
+
+    let statistic_to_json i s =
+      let idx = string_of_int (i + 1) in
+      "Statistics.member." ^ idx, show_stat s
+    in
+    let dimension_to_json i (d,v) =
+      let idx = string_of_int (i + 1) in
+      ["Dimensions.member." ^ idx ^ ".Name", d;
+       "Dimensions.member." ^ idx ^ ".Value", v]
+    in
+
+    let query = [
+        "Namespace", namespace;
+        "MetricName", metric_name;
+        "StartTime", start_time;
+        "EndTime", end_time;
+        "Period", string_of_int period]
+      @ action "GetMetricStatistics"
+      @ List.flatten (List.mapi dimension_to_json dimensions)
+      @ List.mapi statistic_to_json statistics
+    in
+
+    let%lwt res, _ = Service.request ~meth:`POST ~uri:"/" ~query () in
+
+    let tree =
+      let el ((_, tag), _) children = `Tag (tag, children)
+      and data d = `Data d in
+      snd @@ Xmlm.input_doc_tree ~el ~data @@ Xmlm.make_input (`String (0, res))
+    in
+
+    let parse_response =
+      let field = function `Tag (t, [`Data d]) -> Some (t, d) | _ -> None in
+      let member = function
+        | `Tag ("member", xs) -> Some (List.filter_map field xs)
+        | _ -> None
+      in
+      let datapoints = function
+        | `Tag ("Datapoints", xs) -> Some (List.filter_map member xs)
+        | _ -> None
+      in
+      let result = function
+        | `Tag ("GetMetricStatisticsResult", xs) ->
+            Some (List.flatten @@ List.filter_map datapoints xs)
+        | _ -> None
+      in
+      function
+      | `Tag ("GetMetricStatisticsResponse", xs) ->
+          List.flatten @@ List.filter_map result xs
+      | _ -> assert false
+    in
+
+    Lwt.return @@ parse_response tree
+
+end
