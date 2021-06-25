a8fdee64ff235ffcd2bb7b8389154fd7e419c543
Jan Rochel <jrochel@besport.com>
2021-06-25 16:06:00 +0200

    describe-table

--- lib/dynamodb.ml
+++ lib/dynamodb.ml
@@ -9,4 +9,16 @@ module Make (Conf : Service.CONF) = struct
         let name = "dynamodb"
         and host = hostname Conf.region
       end)
+
+  let describe_table ~table_name =
+    let headers =
+      [ "x-amz-target", "DynamoDB_20120810.DescribeTable"
+      ; "content-type", "application/x-amz-json-1.1" ]
+    in
+    let body = ["TableName", `String table_name] in
+    let payload = Yojson.Safe.to_string (`Assoc body) in
+    let%lwt response_body, _ =
+      Service.request ~meth:`POST ~uri:"/" ~headers ~payload ()
+    in
+    Lwt.return @@ Yojson.Basic.from_string response_body
 end
