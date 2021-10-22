62861c026c11e2cc813da2e48047abb3153ce419
Jan Rochel <jrochel@besport.com>
2021-10-22 14:22:52 +0200

    dynamodb

--- lib/dynamodb.ml
+++ lib/dynamodb.ml
@@ -1,6 +1,11 @@
-exception Parse_error
+open Batteries
+
+let todo = Obj.magic @@ fun () -> failwith "TODO"
 
 module Make (Conf : Service.CONF) = struct
+  exception Parse_error
+  exception ResourceInUseException
+
   let hostname region =
     Format.sprintf "dynamodb.%s.amazonaws.com" (Common.Region.to_string region)
 
@@ -11,6 +16,12 @@ module Make (Conf : Service.CONF) = struct
         let name = "dynamodb"
         and host = hostname Conf.region
       end)
+
+  module Aux = struct
+    let single t_of_yojson yojson_of_t =
+      ( (fun json -> t_of_yojson @@ `List [json])
+      , fun t -> match yojson_of_t t with `List [x] -> x | _ -> assert false )
+  end
 
   type attribute_value =
     | B of string
@@ -23,21 +34,24 @@ module Make (Conf : Service.CONF) = struct
     | NULL
     | S of string
     | SS of string list
-  [@@deriving yojson]
+  [@@deriving yojson, show]
 
   let yojson_of_attribute_value =
     let obj_of_list = function
       | `List [`String x; y] -> `Assoc [x, y]
-      | _ -> assert false
+      | _ -> raise Parse_error
     in
-    fun x -> obj_of_list @@ yojson_of_attribute_value x
+    obj_of_list % yojson_of_attribute_value
 
   let attribute_value_of_yojson =
     let list_of_obj = function
       | `Assoc [(x, y)] -> `List [`String x; y]
-      | _ -> assert false
+      | _ -> raise Parse_error
     in
-    fun x -> attribute_value_of_yojson @@ list_of_obj x
+    attribute_value_of_yojson % list_of_obj
+
+  let yojson_of_attribute_values l =
+    `Assoc (List.map (Tuple2.map2 yojson_of_attribute_value) l)
 
   let perform ~action ~payload =
     let headers =
@@ -50,7 +64,7 @@ module Make (Conf : Service.CONF) = struct
     Lwt.return @@ Yojson.Safe.from_string response_body
 
   module GetItem = struct
-    type expressionAttributeNames = (string * string) list
+    type expressionAttributeNames = (string * string) list [@@deriving show]
 
     let expressionAttributeNames_of_yojson = function
       | `Assoc l ->
@@ -64,16 +78,11 @@ module Make (Conf : Service.CONF) = struct
     let yojson_of_expressionAttributeNames l =
       `Assoc (List.map (fun (name, value) -> name, `String value) l)
 
-    type attribute_values = (string * attribute_value) list
-
-    let tuple2_map2 f (x, y) = x, f y
+    type attribute_values = (string * attribute_value) list [@@deriving show]
 
     let attribute_values_of_yojson = function
-      | `Assoc l -> List.map (tuple2_map2 attribute_value_of_yojson) l
+      | `Assoc l -> List.map (Tuple2.map2 attribute_value_of_yojson) l
       | _ -> raise Not_found
-
-    let yojson_of_attribute_values l =
-      `Assoc (List.map (tuple2_map2 yojson_of_attribute_value) l)
 
     type request =
       { attributesToGet : string list option
@@ -87,18 +96,18 @@ module Make (Conf : Service.CONF) = struct
       ; returnConsumedCapacity : string option
             [@yojson.option] [@key "ReturnConsumedCapacity"]
       ; tableName : string [@key "TableName"] }
-    [@@deriving yojson]
+    [@@deriving yojson, show]
 
-    type consumedCapacity = Yojson.Safe.t
+    type consumedCapacity = Yojson.Safe.t [@@deriving show]
 
     let consumedCapacity_of_yojson, yojson_of_consumedCapacity =
-      (fun x -> x), fun x -> x
+      identity, identity
 
     type response =
       { consumedCapacity : consumedCapacity option
             [@yojson.option] [@key "consumedCapacity"]
       ; item : attribute_values option [@yojson.option] [@key "Item"] }
-    [@@deriving yojson]
+    [@@deriving yojson, show]
 
     let request ?attributesToGet ?consistentRead
         ?(expressionAttributeNames = []) ?projectionExpression
@@ -112,7 +121,7 @@ module Make (Conf : Service.CONF) = struct
       ; projectionExpression
       ; returnConsumedCapacity
       ; tableName }
-      [@@deriving yojson]
+      [@@deriving yojson, show]
   end
 
   let get_item ?attributesToGet ?consistentRead ?projectionExpression
@@ -126,10 +135,82 @@ module Make (Conf : Service.CONF) = struct
     in
     let%lwt response = perform ~action:"GetItem" ~payload in
     let {consumedCapacity; item} = response_of_yojson response in
-    Lwt.return (consumedCapacity, Option.value ~default:[] item)
+    Lwt.return (consumedCapacity, Option.default [] item)
+
+  let put_item ~tableName items =
+    let item = yojson_of_attribute_values items in
+    let body = ["TableName", `String tableName; "Item", item] in
+    let payload = Yojson.Safe.to_string (`Assoc body) in
+    perform ~action:"PutItem" ~payload
 
   let describe_table ~tableName =
     let body = ["TableName", `String tableName] in
     let payload = Yojson.Safe.to_string (`Assoc body) in
     perform ~action:"DescribeTable" ~payload
+
+  module AttributeDefinition = struct
+    type attributeType = [`S | `N | `B] [@@deriving yojson, show]
+
+    let attributeType_of_yojson, yojson_of_attributeType =
+      Aux.single attributeType_of_yojson yojson_of_attributeType
+
+    type t =
+      { attributeName : string [@key "AttributeName"]
+      ; attributeType : attributeType [@key "AttributeType"] }
+    [@@deriving yojson, show]
+  end
+
+  module KeySchemaElement = struct
+    type keyType = [`HASH | `RANGE] [@@deriving yojson, show]
+
+    let keyType_of_yojson, yojson_of_keyType =
+      Aux.single keyType_of_yojson yojson_of_keyType
+
+    type t =
+      { attributeName : string [@key "AttributeName"]
+      ; keyType : keyType [@key "KeyType"] }
+    [@@deriving yojson, show]
+  end
+
+  module CreateTable = struct
+    type request =
+      { attributeDefinitions : AttributeDefinition.t list
+            [@key "AttributeDefinitions"]
+      ; keySchema : KeySchemaElement.t list [@key "KeySchema"]
+      ; tableName : string [@key "TableName"]
+      ; billingMode : string [@key "BillingMode"] }
+    [@@deriving yojson, show]
+  end
+
+  let create_table ~tableName ~attributes ~primary_key ?sort_key () =
+    let attributeDefinitions =
+      let attribute_definition (attributeName, attributeType) =
+        {AttributeDefinition.attributeName; attributeType}
+      in
+      List.map attribute_definition attributes
+    and keySchema =
+      let primary_schema_element =
+        {KeySchemaElement.attributeName = primary_key; keyType = `HASH}
+      and sort_key_element =
+        sort_key
+        |> Option.map @@ fun sk ->
+           {KeySchemaElement.attributeName = sk; keyType = `RANGE}
+      in
+      match sort_key_element with
+      | None -> [primary_schema_element]
+      | Some ske -> [primary_schema_element; ske]
+    in
+    let payload =
+      Yojson.Safe.to_string @@ CreateTable.yojson_of_request
+      @@ { CreateTable.attributeDefinitions
+         ; keySchema
+         ; tableName
+         ; billingMode = "PAY_PER_REQUEST" }
+    in
+    try%lwt perform ~action:"CreateTable" ~payload with
+    | Common.Error
+        { code = 400
+        ; typ = "com.amazonaws.dynamodb.v20120810#ResourceInUseException" } ->
+        Lwt.fail ResourceInUseException
+    | exn -> Lwt.fail exn
 end
